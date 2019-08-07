bb08e28718f0e8451540ba73d00a2215d83a9923
Mark Johnson <mark@spideywebdesign.co.uk>
2019-08-07 09:40:18 +0100

    :construction: Added demo for extending models in Models Builder plus some route hijacking example. Renamed solution to be more generic.

---
+++ src/CoreProject/Helpers/IOCHelper.cs
@@ -0,0 +1,46 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using CoreProject.Events;
+using System.Web.Http;
+using System.Web.Mvc;
+using Umbraco.Core;
+using Umbraco.Web;
+
+namespace CoreProject.Helpers
+{
+    public class IocHelper
+    {
+        public static void Register()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterInstance(ApplicationContext.Current).AsSelf();
+
+            // register all controllers found in your assembly
+            builder.RegisterControllers(typeof(UmbracoEvents).Assembly);
+            builder.RegisterApiControllers(typeof(UmbracoEvents).Assembly);
+
+
+            // register Umbraco MVC + web API controllers used by the admin site
+            builder.RegisterControllers(typeof(UmbracoApplication).Assembly);
+            builder.RegisterApiControllers(typeof(UmbracoApplication).Assembly);
+
+            //builder.RegisterApiControllers(typeof(Umbraco.Forms.Web.Trees.FormTreeController).Assembly);
+            builder.RegisterApiControllers(typeof(Terratype.Controllers.AjaxController).Assembly);
+
+            //Fetch all services from assembly based on naming convention "EndsWith" Service or Strategy
+            builder.RegisterAssemblyTypes(typeof(UmbracoEvents).Assembly)
+                .Where(t => t.Name.EndsWith("Service"))
+                .AsImplementedInterfaces()
+                .SingleInstance();
+
+            //builder.RegisterType<RobotsService>().As<IRobotsService>().SingleInstance();
+            //builder.RegisterType<AutoNodeGeneratorService>().As<IAutoNodeGeneratorService>().SingleInstance();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
---
+++ src/CoreProject/Services/BlogService.cs
@@ -0,0 +1,19 @@
+﻿using CoreProject.Services.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Web;
+using ContentModels = CoreProject.Models.Published;
+
+namespace CoreProject.Services
+{
+    public class BlogService : IBlogService
+    {
+        public IEnumerable<ContentModels.Blogpost> GetAllBlogPosts()
+        {
+            var blogPostsXPath = $"//{ContentModels.Home.ModelTypeAlias} [@isDoc]/{ContentModels.Blog.ModelTypeAlias} [@isDoc]/{ContentModels.Blogpost.ModelTypeAlias} [@isDoc]";
+            var uHelper = new UmbracoHelper(UmbracoContext.Current);
+            var blogPosts = uHelper.TypedContentAtXPath(blogPostsXPath).Select(x => new ContentModels.Blogpost(x));
+            return blogPosts ?? Enumerable.Empty<ContentModels.Blogpost>();
+        }
+    }
+}
