723b93b77dab68e38a1329178b57447d1de9da33
Mark Johnson <mark.johnson@connellsgroup.co.uk>
2019-10-02 12:33:15 +0100

    New customer property editor with optional default configuration added.

---
+++ src/WebProject/App_Plugins/My_Awesome_PropertyEditor_With_Config/js/propertysliderwithconfig.resources.js
@@ -0,0 +1,13 @@
+﻿angular.module("umbraco.resources").factory("propertySliderWithConfigResources", function($http) {
+    let myService = {};
+
+    myService.getSalesTypes = function () {
+        return $http.get("/App_Plugins/My_Awesome_PropertyEditor_With_Config/resourcefiles/SalesTypes.json");
+    };
+
+    myService.getSearchParams = function() {
+        return $http.get("/App_Plugins/My_Awesome_PropertyEditor_With_Config/resourcefiles/SearchParams.json");
+    };
+
+    return myService;
+});
